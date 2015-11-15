a1048dd5e3b30752a0fee3563873318a4b702ab1
heimonsy <heimonsy@gmail.com>
2015-11-15 21:55:31 +0800

    myself fish

--- config.fish
+++ config.fish
@@ -1,8 +1,12 @@
 set -x LANG en_US.UTF-8
+# set -x JAVA_HOME /usr/lib/jvm/java-8-oracle
 set -x LANGUAGE en_US.UTF-8
 set -x LC_TYPE en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
 set -x FISH_PATH $HOME/.config/fish
+set -x GOROOT /usr/local/go
 set -x GOPATH $HOME/Develop/go
+# set -x GOPATH $HOME/Develop/go ./vendor ./
 set -x PATH /usr/local/sbin $PATH
-set -x PATH $PATH $GOPATH/bin
+set -x PATH $PATH $GOROOT/bin $HOME/Develop/go/bin $HOME/bin $HOME/.composer/vendor/bin
+set -x TERM xterm-256color
