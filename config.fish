9a140fa5d97f4f1d8b428fa9f24d357ed882ba73
heimonsy <heimonsy@gmail.com>
2018-12-15 11:40:13 +0800

    new

--- config.fish
+++ config.fish
@@ -7,14 +7,13 @@ set -x FISH_PATH $HOME/.config/fish
 set -x GOROOT /usr/local/go
 set -x GOPATH $HOME/Develop/go
 # set -x GOPATH $HOME/Develop/go ./vendor ./
-set -x PATH $PATH $GOROOT/bin $HOME/Develop/go/bin $HOME/bin
-set -x PATH /usr/local/sbin $PATH
+set -x PATH $GOROOT/bin $HOME/Develop/go/bin $HOME/bin $PATH
 set -x TERM xterm-256color
 
 set -g theme_date_format "+%Y-%m-%d %H:%M:%S"
-set -g fish_user_paths "/usr/local/opt/bison/bin" $fish_user_paths
+# set -g fish_user_paths "/usr/local/opt/bison/bin" $fish_user_paths
 
-set -x DC_DB_HOST 192.168.99.101
-set -x DC_REDIS_HOST 192.168.99.101:6379
+#set -x DC_DB_HOST 192.168.99.101
+#set -x DC_REDIS_HOST 192.168.99.101:6379
 set -x DC_MYSQL_PORTS_MAPPING 0.0.0.0:3306:3306
 set -x DC_REDIS_PORTS_MAPPING 0.0.0.0:6379:6379
